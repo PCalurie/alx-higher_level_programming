eea4df4f149f448ffd22a2d1ea9dbd2914c03221
PCalurie <peterkaruri1999@gmail.com>
2023-06-09 06:10:32 -0700

    prints all the names defined

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for entry in dir(hidden_4):
+        if entry[0] != '_':
+            print("{}".format(entry))
