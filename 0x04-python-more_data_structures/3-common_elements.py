ee215f51d5f8d0965c24731c6a4f40c8247c204d
PCalurie <peterkaruri1999@gmail.com>
2023-06-15 06:31:06 -0700

    returns the sum of common elements in two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
 def common_elements(set_1, set_2):
-    result = set()
-    for i in set_1:
-        if i in set_2:
-            result.add(i)
-        return result
+    set_3 = set_1 & set_2
+    return (set_3)
