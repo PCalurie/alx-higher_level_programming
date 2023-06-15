4a3a2c62ea835bd7676be0d159efc447273238b4
PCalurie <peterkaruri1999@gmail.com>
2023-06-14 23:01:19 -0700

    square matrix elements by themselves

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []:
+    new_matrix = []
 
     for row in matrix:
-        new_row = []:
+        new_row = []
 
         for x in row:
             new_row.append(x ** 2)
