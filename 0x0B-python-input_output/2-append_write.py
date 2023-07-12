211ac46346c60dbc9985d1cb2591df92ea61d33f
PCalurie <peterkaruri1999@gmail.com>
2023-07-12 12:34:45 -0700

    reads, appends and creats if it doesn't exist

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -16,5 +16,5 @@ def append_write(filename="", text=""):
 
     """
     
-    with open(filename, 'a', encode="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
