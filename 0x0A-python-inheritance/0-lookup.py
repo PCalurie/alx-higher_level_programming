75cb4db6d835902be7aea309d8371acedbd9ad08
PCalurie <peterkaruri1999@gmail.com>
2023-07-11 23:06:38 -0700

    retrieve elements of an obj dir

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"
-A function that return the list of available attributes and methods of an object
+"""
+A function that returns the list of available attributes
+and methods of an object
 """
 
 
