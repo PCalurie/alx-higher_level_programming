aaf7bd52b071e6190baf87b084b1dd57a77e4057
PCalurie <peterkaruri1999@gmail.com>
2023-07-11 23:03:06 -0700

    retrieve elements of an obj dir

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"
+A function that return the list of available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    This function returns the list of attributes and methods of and obj
+
+    Args:
+        obj: The object to get the attributes and methods from.
+
+    Returns:
+        A list containing the attributes and methods of the object
+    """
+    # create an empty list to store attributes of the object.
+    elements = []
+    # loop through the elements of in the object
+    for element in dir(obj):
+        # append the element to the list
+        names.append(name)
+    # return the list of elements
+    return names
