734026d9ed8df48cd46c2d39ac4e73dc6b873d29
PCalurie <peterkaruri1999@gmail.com>
2023-07-13 06:04:18 -0700

    read command line argvs and save them

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""Script that adds all arguments to a Python list, then saves them to a file
+in a specified serialization formtat"""
+
+
 import sys
+from os import path
 
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
-try:
-    items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    items = []
+# Import functions from the previous tasks
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-items.extend(sys.argv[1:])
-save_to_json_file(items, "add_item.json")
+
+# The name of the file to be saved to
+filename = "add_item.json"
+
+# If the file exists, load the list from it, otherwise create an empty list
+if path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+# Add all the arguments passed to the list
+my_list.extend(sys.argv[1:])
+
+# Save the list as a JSON representation in the file
+save_to_json_file(my_list, filename)
